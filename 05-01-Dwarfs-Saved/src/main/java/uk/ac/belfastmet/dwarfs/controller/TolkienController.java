e74cecc12063f304cae27190a6db7b5b804421ad
Will Parks (PAR18161671) <PAR18161671@belfastmet.ac.uk>
2018-12-04 13:28:05 +0000

    Added search bars into the form

--- 05-01-Dwarfs-Saved/src/main/java/uk/ac/belfastmet/dwarfs/controller/TolkienController.java
+++ 05-01-Dwarfs-Saved/src/main/java/uk/ac/belfastmet/dwarfs/controller/TolkienController.java
@@ -1,15 +1,14 @@
 package uk.ac.belfastmet.dwarfs.controller;
 
-import java.util.ArrayList;
-
+//import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import uk.ac.belfastmet.dwarfs.domain.Dwarf;
+//import uk.ac.belfastmet.dwarfs.domain.Dwarf;
+//import uk.ac.belfastmet.dwarfs.service.DwarfService;
 import uk.ac.belfastmet.dwarfs.repository.DwarfRepository;
-import uk.ac.belfastmet.dwarfs.service.DwarfService;
 
 @Controller
 @RequestMapping
--- 05-01-Dwarfs-Saved/src/main/java/uk/ac/belfastmet/dwarfs/repository/DwarfRepository.java
+++ 05-01-Dwarfs-Saved/src/main/java/uk/ac/belfastmet/dwarfs/repository/DwarfRepository.java
@@ -7,5 +7,11 @@ import uk.ac.belfastmet.dwarfs.domain.Dwarf;
 public interface DwarfRepository extends CrudRepository<Dwarf, Integer> {
 
 	Iterable<Dwarf> findByAuthor(String author);
+
+	Dwarf findByDwarfId(Integer dwarfId);
+
+	Iterable<Dwarf> findByName(String name);
+
+	Dwarf findByNameAndAuthor(String name, String author);
 	
 }
