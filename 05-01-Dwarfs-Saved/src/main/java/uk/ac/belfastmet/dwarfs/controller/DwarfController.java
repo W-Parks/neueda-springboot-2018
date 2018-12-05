ad30aae531ac2e0f2daeeb71d57381f0487f6695
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-05 12:24:34 +0000

    Added view page and button.

---
+++ 05-01-Dwarfs-Saved/src/main/java/uk/ac/belfastmet/dwarfs/controller/DwarfController.java
@@ -0,0 +1,30 @@
+package uk.ac.belfastmet.dwarfs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import uk.ac.belfastmet.dwarfs.repository.DwarfRepository;
+
+@Controller
+@RequestMapping()
+public class DwarfController {
+
+	DwarfRepository dwarfRepository;
+
+	public DwarfController(DwarfRepository dwarfRepository) {
+		super();
+		this.dwarfRepository = dwarfRepository;
+	}
+	
+	@GetMapping("/view/{dwarfId}")
+	public String viewDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model) {
+		model.addAttribute("pageTitle", "View");
+		model.addAttribute("dwarf", this.dwarfRepository.findByDwarfId(dwarfId));
+		return "viewDwarf";
+	}
+	
+	
+}
