e02ba095b9f74460f7259dbaa4bdb44699b566b3
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-05 14:02:50 +0000

    Search Function implemented for each field on table

--- 05-02-TitanicCrud/src/main/java/uk/ac/belfastmet/TitanicCrud/controller/SearchController.java
+++ 05-02-TitanicCrud/src/main/java/uk/ac/belfastmet/TitanicCrud/controller/SearchController.java
@@ -21,17 +21,87 @@ public class SearchController {
 	}
 	
 	@GetMapping("/searchid")
-	public String searchId(@RequestParam("id") Integer dwarfId, Model model) {		
+	public String searchId(@RequestParam("id") Integer passengerId, Model model) {		
 		model.addAttribute("pageTitle", "Search!");
 		model.addAttribute("passengers", this.passengerRepository.findByPassengerId(passengerId));
-		return "passengers";
+		return "passengersPage";
 	}
 	
 	@PostMapping("/searchname")
 	public String searchName(@RequestParam("name") String name, Model model) {		
 		model.addAttribute("pageTitle", "Search!");
 		model.addAttribute("dwarfs", this.passengerRepository.findByName(name));
-		return "passenegrs";
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchsurvived")
+	public String searchSurvived(@RequestParam("survived") Boolean survived, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findBySurvived(survived));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchpclass")
+	public String searchPClass(@RequestParam("pclass") Integer pClass, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findByPClass(pClass));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchsex")
+	public String searchSex(@RequestParam("sex") String sex, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findBySex(sex));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchage")
+	public String searchAge(@RequestParam("age") Integer age, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findByAge(age));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchsibsp")
+	public String searchSibSp(@RequestParam("sibsp") Integer sibSp, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findBySibSp(sibSp));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchparch")
+	public String searchParch(@RequestParam("parch") Integer parch, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findByParch(parch));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchticket")
+	public String searchTicket(@RequestParam("ticket") String ticket, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findByTicket(ticket));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchfare")
+	public String searchFare(@RequestParam("fare") Double fare, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findByFare(fare));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchcabin")
+	public String searchCabin(@RequestParam("cabin") String cabin, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findByCabin(cabin));
+		return "passengersPage";
+	}
+	
+	@GetMapping("/searchembarked")
+	public String searchEmbarked(@RequestParam("embarked") String embarked, Model model) {		
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("passengers", this.passengerRepository.findByEmbarked(embarked));
+		return "passengersPage";
 	}
 	
 }
--- 05-02-TitanicCrud/src/main/java/uk/ac/belfastmet/TitanicCrud/repository/PassengerRepository.java
+++ 05-02-TitanicCrud/src/main/java/uk/ac/belfastmet/TitanicCrud/repository/PassengerRepository.java
@@ -5,5 +5,31 @@ import org.springframework.data.repository.CrudRepository;
 import uk.ac.belfastmet.TitanicCrud.domain.Passenger;
 
 public interface PassengerRepository extends CrudRepository<Passenger, Integer> {
+	
+	Passenger findByPassengerId(Integer passengerId);
+	
+	Iterable<Passenger> findByName(String name);
+
+	Iterable<Passenger> findBySurvived(Boolean survived);
+
+	Iterable<Passenger> findByPClass(Integer pClass);
+
+	Iterable<Passenger> findBySex(String sex);
+
+	Iterable<Passenger> findByAge(Integer age);
+
+	Iterable<Passenger> findBySibSp(Integer sibSp);
+
+	Iterable<Passenger> findByParch(Integer parch);
+
+	Iterable<Passenger> findByTicket(String ticket);
+
+	Iterable<Passenger> findByFare(Double fare);
+
+	Iterable<Passenger> findByCabin(String cabin);
+
+	Iterable<Passenger> findByEmbarked(String embarked);
+
+	
 
 }
