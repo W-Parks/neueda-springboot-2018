cfcbd4c597fb2ea292792e43533345e37c399975
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-06 14:25:11 +0000

    Added CRUD features to TitanicCrud

--- 05-02-TitanicCrud/src/main/java/uk/ac/belfastmet/TitanicCrud/controller/PassengerController.java
+++ 05-02-TitanicCrud/src/main/java/uk/ac/belfastmet/TitanicCrud/controller/PassengerController.java
@@ -1,13 +1,18 @@
 package uk.ac.belfastmet.TitanicCrud.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.belfastmet.TitanicCrud.repository.PassengerRepository;
+import uk.ac.belfastmet.TitanicCrud.domain.Passenger;
 
 @Controller
 @RequestMapping
@@ -27,6 +32,33 @@ public class PassengerController {
 		return "passengersPage";
 	}
 
+	@GetMapping("/add/")
+	public String addPassenger(Model model) {
+		
+		model.addAttribute("pageTitle", "Add");
+		model.addAttribute("passenger", new Passenger());
+		return "editPassengers";
+	}
+	
+	@GetMapping("/edit/{passengerId}")
+	public String editPassenger(@PathVariable("passengerId") Integer passengerId, Model model) {
+		
+		model.addAttribute("pageTitle", "Edit");
+		model.addAttribute("dwarf", this.passengerRepository.findByPassengerId(passengerId));
+		return "editDwarf";
+	}
+	
+	@PostMapping("/save")
+	public String savePassenegr(@Valid Passenger passenger, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors())
+		{
+			return "editPassengers";
+		}else {
+			Passenger savePassenger = this.passengerRepository.save(passenger);
+			return "redirect:/view/"+ passenger.getPassengerId();
+		}
+	}
+	
 	@GetMapping("/view/{passengerId}")
 	public String viewPassenger(@PathVariable("passengerId") Integer passengerId, Model model) {
 		model.addAttribute("pageTitle", "View");
