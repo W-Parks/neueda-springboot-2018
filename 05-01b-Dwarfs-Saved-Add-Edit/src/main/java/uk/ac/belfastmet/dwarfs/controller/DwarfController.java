9f672e1bb4cbc791fb9339d6e51d28364accffe3
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-06 10:14:40 +0000

    Implemented CRUD Add function

--- 05-01b-Dwarfs-Saved-Add-Edit/src/main/java/uk/ac/belfastmet/dwarfs/controller/DwarfController.java
+++ 05-01b-Dwarfs-Saved-Add-Edit/src/main/java/uk/ac/belfastmet/dwarfs/controller/DwarfController.java
@@ -2,12 +2,15 @@ package uk.ac.belfastmet.dwarfs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.belfastmet.dwarfs.repository.DwarfRepository;
+import uk.ac.belfastmet.dwarfs.domain.Dwarf;
 
 @Controller
 @RequestMapping()
@@ -22,13 +25,35 @@ public class DwarfController {
 	
 	@GetMapping("/dwarfs")
 	public String Dwarf(Model model) {
+		
 		model.addAttribute("pageTitle", "All");
 		model.addAttribute("dwarfs", this.dwarfRepository.findAll());
 		return "dwarf";
 	}
 	
+	@GetMapping("/add/")
+	public String addDwarf(Model model) {
+		
+		model.addAttribute("pageTitle", "Add");
+		model.addAttribute("dwarf", new Dwarf());
+		return "editDwarf";
+	}
+	
+	@PostMapping("/save")
+	public String saveDwarf(Dwarf dwarf, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors())
+		{
+			return "editDwarf";
+		}else {
+			Dwarf saveDwarf = this.dwarfRepository.save(dwarf);
+			return "redirect:/view/"+ dwarf.getDwarfId();
+		}
+
+	}
+	
 	@GetMapping("/view/{dwarfId}")
 	public String viewDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model) {
+		
 		model.addAttribute("pageTitle", "View");
 		model.addAttribute("dwarf", this.dwarfRepository.findByDwarfId(dwarfId));
 		return "viewDwarf";
@@ -41,6 +66,5 @@ public class DwarfController {
 		redirectAttributes.addFlashAttribute("message", "Dwarf Deleted Successfully");
 		return "redirect:/dwarfs";
 	}
-	
-	
+		
 }
