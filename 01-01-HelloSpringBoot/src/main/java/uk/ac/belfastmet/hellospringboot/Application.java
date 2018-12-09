59bc8e75f2aa31469c824751a18d73e13973b0d2
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-09 00:05:42 +0000

    Removed test comment

--- 01-01-HelloSpringBoot/src/main/java/uk/ac/belfastmet/hellospringboot/Application.java
+++ 01-01-HelloSpringBoot/src/main/java/uk/ac/belfastmet/hellospringboot/Application.java
@@ -13,7 +13,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		//Test comment
-		//Test comment 2
 		logger.info("Hello Spring Boot");
 	}
 }
