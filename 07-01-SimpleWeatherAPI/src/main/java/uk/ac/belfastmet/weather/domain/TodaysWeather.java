56018a3e215e1c9d7457ec285e98385723c9cde3
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-12 10:20:42 +0000

    Weather information prints to console

--- 07-01-SimpleWeatherAPI/src/main/java/uk/ac/belfastmet/weather/domain/TodaysWeather.java
+++ 07-01-SimpleWeatherAPI/src/main/java/uk/ac/belfastmet/weather/domain/TodaysWeather.java
@@ -1,13 +1,21 @@
 package uk.ac.belfastmet.weather.domain;
 
+import java.util.Map;
+
 import lombok.Data;
 
 @Data
 public class TodaysWeather {
 
 	private Location location;
-	private Weather weather;
+	private Map<String, Object> weather;
 	
-	
+	public String toString() {
+		
+		String location = "\n" + this.location.getName() + " , " + this.location.getLatitude() + " , " + this.location.getLongitude() + "\n";
+		
+		String weather = this.weather.toString();
+		return location + weather;
+	}
 	
 }
